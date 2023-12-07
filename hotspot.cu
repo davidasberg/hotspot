d3465f210c01a2c5abce2ccc34d9c7d147b00a5a
moscars <marcus.oscarsson9@gmail.com>
2023-12-07 11:22:49 +0100

    formatting and added timer func

--- hotspot.cu
+++ hotspot.cu
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
+#include <iostream>
+#include <sys/time.h>
 
 #ifdef RD_WG_SIZE_0_0                                                            
-        #define BLOCK_SIZE RD_WG_SIZE_0_0                                        
+    #define BLOCK_SIZE RD_WG_SIZE_0_0                                        
 #elif defined(RD_WG_SIZE_0)                                                      
-        #define BLOCK_SIZE RD_WG_SIZE_0                                          
+    #define BLOCK_SIZE RD_WG_SIZE_0                                          
 #elif defined(RD_WG_SIZE)                                                        
-        #define BLOCK_SIZE RD_WG_SIZE                                            
+    #define BLOCK_SIZE RD_WG_SIZE                                            
 #else                                                                                    
-        #define BLOCK_SIZE 16                                                            
+    #define BLOCK_SIZE 16                                                            
 #endif                                                                                   
 
 #define STR_SIZE 256
@@ -34,67 +36,61 @@ float amb_temp = 80.0;
 void run(int argc, char** argv);
 
 /* define timer macros */
-#define pin_stats_reset()   startCycle()
-#define pin_stats_pause(cycles)   stopCycle(cycles)
-#define pin_stats_dump(cycles)    printf("timer: %Lu\n", cycles)
+#define pin_stats_reset() startCycle()
+#define pin_stats_pause(cycles) stopCycle(cycles)
+#define pin_stats_dump(cycles) printf("timer: %Lu\n", cycles)
 
+double cpuSecond() {
+    struct timeval tp;
+    gettimeofday(&tp,NULL);
+    return ((double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
+}
 
-
-void 
-fatal(char *s)
-{
+void fatal(char *s){
 	fprintf(stderr, "error: %s\n", s);
-
 }
 
 void writeoutput(float *vect, int grid_rows, int grid_cols, char *file){
-
 	int i,j, index=0;
 	FILE *fp;
 	char str[STR_SIZE];
 
 	if( (fp = fopen(file, "w" )) == 0 )
-          printf( "The file was not opened\n" );
+        printf( "The file was not opened\n" );
 
 
 	for (i=0; i < grid_rows; i++) 
-	 for (j=0; j < grid_cols; j++)
-	 {
-
-		 sprintf(str, "%d\t%g\n", index, vect[i*grid_cols+j]);
-		 fputs(str,fp);
-		 index++;
-	 }
-		
-      fclose(fp);	
+        for (j=0; j < grid_cols; j++){
+            sprintf(str, "%d\t%g\n", index, vect[i*grid_cols+j]);
+            fputs(str,fp);
+            index++;
+    	 }
+    fclose(fp);	
 }
 
 
 void readinput(float *vect, int grid_rows, int grid_cols, char *file){
-
   	int i,j;
 	FILE *fp;
 	char str[STR_SIZE];
 	float val;
 
 	if( (fp  = fopen(file, "r" )) ==0 )
-            printf( "The file was not opened\n" );
+        printf( "The file was not opened\n" );
 
 
 	for (i=0; i <= grid_rows-1; i++) 
-	 for (j=0; j <= grid_cols-1; j++)
-	 {
-		fgets(str, STR_SIZE, fp);
-		if (feof(fp))
-			fatal("not enough lines in file");
-		//if ((sscanf(str, "%d%f", &index, &val) != 2) || (index != ((i-1)*(grid_cols-2)+j-1)))
-		if ((sscanf(str, "%f", &val) != 1))
-			fatal("invalid file format");
-		vect[i*grid_cols+j] = val;
-	}
+        for (j=0; j <= grid_cols-1; j++){
+            fgets(str, STR_SIZE, fp);
+            if (feof(fp))
+                fatal("not enough lines in file");
+            //if ((sscanf(str, "%d%f", &index, &val) != 2) || (index != ((i-1)*(grid_cols-2)+j-1)))
+            if ((sscanf(str, "%f", &val) != 1))
+                fatal("invalid file format");
+            vect[i*grid_cols+j] = val;
+	    }
 
 	fclose(fp);	
-
 }
 
 #define IN_RANGE(x, min, max)   ((x)>=(min) && (x)<=(max))
@@ -116,16 +112,16 @@ __global__ void calculate_temp(int iteration,  //number of iteration
                                float step, 
                                float time_elapsed){
 	
-        __shared__ float temp_on_cuda[BLOCK_SIZE][BLOCK_SIZE];
-        __shared__ float power_on_cuda[BLOCK_SIZE][BLOCK_SIZE];
-        __shared__ float temp_t[BLOCK_SIZE][BLOCK_SIZE]; // saving temparary temperature result
+    __shared__ float temp_on_cuda[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float power_on_cuda[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float temp_t[BLOCK_SIZE][BLOCK_SIZE]; // saving temparary temperature result
 
 	float amb_temp = 80.0;
-        float step_div_Cap;
-        float Rx_1,Ry_1,Rz_1;
+    float step_div_Cap;
+    float Rx_1,Ry_1,Rz_1;
         
 	int bx = blockIdx.x;
-        int by = blockIdx.y;
+    int by = blockIdx.y;
 
 	int tx=threadIdx.x;
 	int ty=threadIdx.y;
@@ -136,75 +132,76 @@ __global__ void calculate_temp(int iteration,  //number of iteration
 	Ry_1=1/Ry;
 	Rz_1=1/Rz;
 	
-        // each block finally computes result for a small block
-        // after N iterations. 
-        // it is the non-overlapping small blocks that cover 
-        // all the input data
+    // each block finally computes result for a small block
+    // after N iterations. 
+    // it is the non-overlapping small blocks that cover 
+    // all the input data
 
-        // calculate the small block size
+    // calculate the small block size
 	int small_block_rows = BLOCK_SIZE-iteration*2;//EXPAND_RATE
 	int small_block_cols = BLOCK_SIZE-iteration*2;//EXPAND_RATE
 
-        // calculate the boundary for the block according to 
-        // the boundary of its small block
-        int blkY = small_block_rows*by-border_rows;
-        int blkX = small_block_cols*bx-border_cols;
-        int blkYmax = blkY+BLOCK_SIZE-1;
-        int blkXmax = blkX+BLOCK_SIZE-1;
+    // calculate the boundary for the block according to 
+    // the boundary of its small block
+    int blkY = small_block_rows*by-border_rows;
+    int blkX = small_block_cols*bx-border_cols;
+    int blkYmax = blkY+BLOCK_SIZE-1;
+    int blkXmax = blkX+BLOCK_SIZE-1;
 
-        // calculate the global thread coordination
+    // calculate the global thread coordination
 	int yidx = blkY+ty;
 	int xidx = blkX+tx;
 
-        // load data if it is within the valid input range
+    // load data if it is within the valid input range
 	int loadYidx=yidx, loadXidx=xidx;
-        int index = grid_cols*loadYidx+loadXidx;
+    int index = grid_cols*loadYidx+loadXidx;
        
 	if(IN_RANGE(loadYidx, 0, grid_rows-1) && IN_RANGE(loadXidx, 0, grid_cols-1)){
-            temp_on_cuda[ty][tx] = temp_src[index];  // Load the temperature data from global memory to shared memory
-            power_on_cuda[ty][tx] = power[index];// Load the power data from global memory to shared memory
+        temp_on_cuda[ty][tx] = temp_src[index];  // Load the temperature data from global memory to shared memory
+        power_on_cuda[ty][tx] = power[index];// Load the power data from global memory to shared memory
 	}
 	__syncthreads();
 
-        // effective range within this block that falls within 
-        // the valid range of the input data
-        // used to rule out computation outside the boundary.
-        int validYmin = (blkY < 0) ? -blkY : 0;
-        int validYmax = (blkYmax > grid_rows-1) ? BLOCK_SIZE-1-(blkYmax-grid_rows+1) : BLOCK_SIZE-1;
-        int validXmin = (blkX < 0) ? -blkX : 0;
-        int validXmax = (blkXmax > grid_cols-1) ? BLOCK_SIZE-1-(blkXmax-grid_cols+1) : BLOCK_SIZE-1;
+    // effective range within this block that falls within 
+    // the valid range of the input data
+    // used to rule out computation outside the boundary.
+    int validYmin = (blkY < 0) ? -blkY : 0;
+    int validYmax = (blkYmax > grid_rows-1) ? BLOCK_SIZE-1-(blkYmax-grid_rows+1) : BLOCK_SIZE-1;
+    int validXmin = (blkX < 0) ? -blkX : 0;
+    int validXmax = (blkXmax > grid_cols-1) ? BLOCK_SIZE-1-(blkXmax-grid_cols+1) : BLOCK_SIZE-1;
 
-        int N = ty-1;
-        int S = ty+1;
-        int W = tx-1;
-        int E = tx+1;
-        
-        N = (N < validYmin) ? validYmin : N;
-        S = (S > validYmax) ? validYmax : S;
-        W = (W < validXmin) ? validXmin : W;
-        E = (E > validXmax) ? validXmax : E;
+    int N = ty-1;
+    int S = ty+1;
+    int W = tx-1;
+    int E = tx+1;
+    
+    N = (N < validYmin) ? validYmin : N;
+    S = (S > validYmax) ? validYmax : S;
+    W = (W < validXmin) ? validXmin : W;
+    E = (E > validXmax) ? validXmax : E;
 
-        bool computed;
-        for (int i=0; i<iteration ; i++){ 
-            computed = false;
-            if( IN_RANGE(tx, i+1, BLOCK_SIZE-i-2) &&  \
-                  IN_RANGE(ty, i+1, BLOCK_SIZE-i-2) &&  \
-                  IN_RANGE(tx, validXmin, validXmax) && \
-                  IN_RANGE(ty, validYmin, validYmax) ) {
-                  computed = true;
-                  temp_t[ty][tx] =   temp_on_cuda[ty][tx] + step_div_Cap * (power_on_cuda[ty][tx] + 
-	       	         (temp_on_cuda[S][tx] + temp_on_cuda[N][tx] - 2.0*temp_on_cuda[ty][tx]) * Ry_1 + 
-		             (temp_on_cuda[ty][E] + temp_on_cuda[ty][W] - 2.0*temp_on_cuda[ty][tx]) * Rx_1 + 
-		             (amb_temp - temp_on_cuda[ty][tx]) * Rz_1);
-	
-            }
-            __syncthreads();
-            if(i==iteration-1)
-                break;
-            if(computed)	 //Assign the computation range
-                temp_on_cuda[ty][tx]= temp_t[ty][tx];
-            __syncthreads();
-          }
+    bool computed;
+    for (int i=0; i<iteration ; i++){ 
+        computed = false;
+        if( IN_RANGE(tx, i+1, BLOCK_SIZE-i-2) &&  \
+                IN_RANGE(ty, i+1, BLOCK_SIZE-i-2) &&  \
+                IN_RANGE(tx, validXmin, validXmax) && \
+                IN_RANGE(ty, validYmin, validYmax) ) {
+                computed = true;
+                temp_t[ty][tx] = temp_on_cuda[ty][tx] + step_div_Cap * (power_on_cuda[ty][tx] + 
+                    (temp_on_cuda[S][tx] + temp_on_cuda[N][tx] - 2.0*temp_on_cuda[ty][tx]) * Ry_1 + 
+                    (temp_on_cuda[ty][E] + temp_on_cuda[ty][W] - 2.0*temp_on_cuda[ty][tx]) * Rx_1 + 
+                    (amb_temp - temp_on_cuda[ty][tx]) * Rz_1);
+
+        }
+        __syncthreads();
+
+        if(i==iteration-1)
+            break;
+        if(computed)	 //Assign the computation range
+            temp_on_cuda[ty][tx]= temp_t[ty][tx];
+        __syncthreads();
+    }
 
       // update the global memory
       // after the last iteration, only threads coordinated within the 
@@ -221,8 +218,8 @@ __global__ void calculate_temp(int iteration,  //number of iteration
 int compute_tran_temp(float *MatrixPower,float *MatrixTemp[2], int col, int row, \
 		int total_iterations, int num_iterations, int blockCols, int blockRows, int borderCols, int borderRows) 
 {
-        dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-        dim3 dimGrid(blockCols, blockRows);  
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 dimGrid(blockCols, blockRows);  
 	
 	float grid_height = chip_height / row;
 	float grid_width = chip_width / col;
@@ -235,23 +232,22 @@ int compute_tran_temp(float *MatrixPower,float *MatrixTemp[2], int col, int row,
 	float max_slope = MAX_PD / (FACTOR_CHIP * t_chip * SPEC_HEAT_SI);
 	float step = PRECISION / max_slope;
 	float t;
-        float time_elapsed;
+    float time_elapsed;
 	time_elapsed=0.001;
 
-        int src = 1, dst = 0;
+    int src = 1, dst = 0;
 	
 	for (t = 0; t < total_iterations; t+=num_iterations) {
-            int temp = src;
-            src = dst;
-            dst = temp;
-            calculate_temp<<<dimGrid, dimBlock>>>(MIN(num_iterations, total_iterations-t), MatrixPower,MatrixTemp[src],MatrixTemp[dst],\
+        int temp = src;
+        src = dst;
+        dst = temp;
+        calculate_temp<<<dimGrid, dimBlock>>>(MIN(num_iterations, total_iterations-t), MatrixPower,MatrixTemp[src],MatrixTemp[dst],\
 		col,row,borderCols, borderRows, Cap,Rx,Ry,Rz,step,time_elapsed);
 	}
-        return dst;
+    return dst;
 }
 
-void usage(int argc, char **argv)
-{
+void usage(int argc, char **argv){
 	fprintf(stderr, "Usage: %s <grid_rows/grid_cols> <pyramid_height> <sim_time> <temp_file> <power_file> <output_file>\n", argv[0]);
 	fprintf(stderr, "\t<grid_rows/grid_cols>  - number of rows/cols in the grid (positive integer)\n");
 	fprintf(stderr, "\t<pyramid_height> - pyramid heigh(positive integer)\n");
@@ -262,17 +258,16 @@ void usage(int argc, char **argv)
 	exit(1);
 }
 
-int main(int argc, char** argv)
-{
-  printf("WG size of kernel = %d X %d\n", BLOCK_SIZE, BLOCK_SIZE);
+int main(int argc, char** argv){   
+    std::cout << "version 2" << std::endl;
+    printf("WG size of kernel = %d X %d\n", BLOCK_SIZE, BLOCK_SIZE);
 
     run(argc,argv);
 
     return EXIT_SUCCESS;
 }
 
-void run(int argc, char** argv)
-{
+void run(int argc, char** argv){
     int size;
     int grid_rows,grid_cols;
     float *FilesavingTemp,*FilesavingPower,*MatrixOut; 
